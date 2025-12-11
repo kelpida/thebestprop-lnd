ea11d0c49f22a893203644f24522b3680b38d283
Elpida <elpida.k@chameleon.com.cy>
2025-12-11 08:20:24 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  preview: {
+    allowedHosts: ["lnd.thebestprop.com"],
+  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
